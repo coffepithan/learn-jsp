7e4e282678edb3920732af0c24750982012e429a
coffepithan <coffepithan@gmail.com>
2024-11-27 17:31:32 -0300

    targets :D

--- src/main/java/model/Login.java
+++ src/main/java/model/Login.java
@@ -2,31 +2,57 @@ package model;
 
 import java.io.Serializable;
 
-public class Login implements Serializable{
+public class Login implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
+	private Long Id;
 	private String login;
 	private String pwd;
-	
-	
-	
+	private String email;
+	private String username;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String name) {
+		this.username = name;
+	}
+
 	public String getLogin() {
 		return login;
 	}
+
 	public void setLogin(String login) {
 		this.login = login;
 	}
+
 	public String getPwd() {
 		return pwd;
 	}
+
 	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
-	
-	
-	
+
 }
